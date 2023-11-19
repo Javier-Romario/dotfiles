d9f4020c87b065e3b801dd4635907dd4fd59f419
Javier Harford <javierharford@Javiers-MacBook-Air.local>
2023-11-19 22:29:48 +0000

    added completions and lazygit

--- nvim/lua/plugins/gen.lua
+++ nvim/lua/plugins/gen.lua
@@ -1,13 +1,15 @@
 return {
-  "David-Kunz/gen.nvim",
-  keys = {
-    { "<leader>ga", ":Gen<CR>", desc = "Ollama Generate", mode = { "n", "v" } }, -- This Works
-    -- { "<leader>ga", "<cmd>Gen<CR>", desc = "Ollama Generate", mode = { "n", "v" } }, -- This does NOT Work
-    -- { "<leader>ga", "Gen<CR>", desc = "Ollama Generate", mode = { "n", "v" } }, -- This does NOT Work
+  {
+    "David-Kunz/gen.nvim",
+    keys = {
+      { "<leader>ga", ":Gen<CR>", desc = "Ollama Generate", mode = { "n", "v" } }, -- This Works
+      -- { "<leader>ga", "<cmd>Gen<CR>", desc = "Ollama Generate", mode = { "n", "v" } }, -- This does NOT Work
+      -- { "<leader>ga", "Gen<CR>", desc = "Ollama Generate", mode = { "n", "v" } }, -- This does NOT Work
+    },
+    config = function(_, _)
+      -- vim.keymap.set("n", "<leader>ga", ":Gen<CR>", { silent = true }) -- This Works
+      -- vim.keymap.set("v", "<leader>ga", ":Gen<CR>", { silent = true }) -- This Works
+      require("gen").model = "llama2"
+    end,
   },
-  config = function(_, _)
-    -- vim.keymap.set("n", "<leader>ga", ":Gen<CR>", { silent = true }) -- This Works
-    -- vim.keymap.set("v", "<leader>ga", ":Gen<CR>", { silent = true }) -- This Works
-    require("gen").model = "llama2"
-  end,
 }
